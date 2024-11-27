07bb07194a2af8095f72bb4a9a8ce2ef7652797c
Marine1651 <123360929+Marineseeker@users.noreply.github.com>
2024-11-27 20:17:27 +0800

    解决老旧api的warning问题

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
-import path from 'node:path'
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueDevTools from 'vite-plugin-vue-devtools';
+import path from 'node:path';
+import dartSass from 'sass';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -17,13 +17,21 @@ export default defineConfig({
     },
   },
   // 使用代理处理跨域问题
-  server:{
-    proxy:{
-      '/api':{// 任何以 /api 开头的请求都会被代理
-        target:'http://localhost:8080',
-        changeOrigin:true,
-        rewrite:path=>path.replace(/^\/api/,'')
+  server: {
+    proxy: {
+      '/api': { // 任何以 /api 开头的请求都会被代理
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, '')
       }
     }
-  }
-})
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // 使用现代api
+        api: 'modern-compiler'
+      },
+    },
+  },
+});
